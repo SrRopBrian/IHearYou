a4903329405d8802b5815a5ecf50b2fa7b326e89
SrRopBrian <kipngenobrian070@gmail.com>
2025-10-06 13:15:54 +0300

    Upgraded AGP versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.example.ihearyou"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ihearyou"
